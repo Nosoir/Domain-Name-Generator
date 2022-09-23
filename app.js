35a5386221dacd65edcbac2ab35cc21ce3cded8d
Nosoir <noeliairace13@gmail.com>
2022-09-23 18:33:01 +0000

    Domain Name Generator

---
+++ app.js
@@ -0,0 +1,21 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let n = 0; n < noun.length; n++) {
+            console.log(pronoun[i].concat(adj[j],noun[n]) + ".com");
+        }
+    }
+}
+
+
+// thegreatjogger.com
+// thegreatracoon.com
+// ourgreatjogger.com
+// ourgreatracoon.com
+// thebigjogger.com
+// thebigracoon.com
+// ourbigjogger.com
+// ourbigracoon.com
